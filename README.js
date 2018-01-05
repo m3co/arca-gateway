e329a2d823feb0a24ef54cabc15aa9b0d6368888
Mikhail Smirnov <urine89@mail.ru>
2018-01-05 16:30:30 +0100

    Update README.md

--- README.js
+++ README.js
@@ -2,11 +2,13 @@
 
 In order to start the back-server, execute the following:
 ```
-npm start
+npm run back
 ```
 
 In order to start the front-server (example), execute the following:
 ```
-http-server
+npm run front
 ```
 Then navigate to http://localhost:8080/client.html
+
+Notice that the servers front and back are running under ports 9001 and 9000 respectively.
