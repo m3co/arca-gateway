1b1edd1f567261f824290527cf67a33bc77e5504
Andres Rios <rianby64@gmail.com>
2018-01-05 03:20:09 +0300

    Haga una conexion TCP por cada conexion Socket.io

--- nodejs-test.js
+++ nodejs-test.js
@@ -1,40 +1,48 @@
 'use strict';
 var net = require('net');
-var arca = new net.Socket();
 var io = require('socket.io')();
 
-arca.sendObject = function sendObject(o) {
-  arca.write(JSON.stringify(o));
-  // THIS IS AN ERROR, BUT ANYWAY... LETS ACCEPT THIS FACT
-  arca.write('\n');
-  // IN FACT, WE'VE TO SEND THE SYMBOL EOF(end-of-file) IN ORDER TO
-  // FLUSH THE TRANSMISSION OF THIS STRING
+function connectToArca() {
+  var arca = new net.Socket();
+  arca.sendObject = function sendObject(o) {
+    arca.write(JSON.stringify(o));
+    // THIS IS AN ERROR, BUT ANYWAY... LETS ACCEPT THIS FACT
+    arca.write('\n');
+    // IN FACT, WE'VE TO SEND THE SYMBOL EOF(end-of-file) IN ORDER TO
+    // FLUSH THE TRANSMISSION OF THIS STRING
+  }
+
+  arca.connect(12345, 'localhost', () => {
+    console.log('connected to ARCA');
+    var e = {
+      query: 'subscribe',
+      module: 'Supplies'
+    };
+    arca.sendObject(e);
+  });
+
+  arca.on('data', data => {
+    console.log('data from ARCA:\n' + data);
+  });
+
+  arca.on('close', () => {
+    console.log('disconnected from ARCA');
+  });
+  return arca;
 }
-
-arca.connect(12345, 'localhost', () => {
-  console.log('connected to ARCA');
-  var e = {
-    query: 'subscribe',
-    module: 'Supplies'
-  };
-  arca.sendObject(e);
-});
-
-
-arca.on('data', data => {
-  console.log('data from ARCA:\n' + data);
-});
-
-
-arca.on('close', () => {
-  console.log('disconnected from ARCA');
-});
 
 io.on('connection', client => {
   console.log('connected');
 
+  var arca = connectToArca();
+
   client.on('data', data => {
     console.log(data);
+  });
+
+  client.on('disconnect', () => {
+    console.log('disconnect');
+    arca.end();
   });
 });
 
