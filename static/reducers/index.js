9d978bc0268a82175e7aa6e410386ebb5badfa11
Михаил Смирнов <urine89@mail.ru>
2018-01-10 19:57:44 +0300

    Delete from reducer

--- static/reducers/index.js
+++ static/reducers/index.js
@@ -1,8 +1,6 @@
 import {combineReducers} from 'redux';
-import {reducer as formReducer} from 'redux-form'
 import rows from 'static/reducers/rows';
 
 export default combineReducers({
-  rows,
-  form: formReducer
+  rows
 })
