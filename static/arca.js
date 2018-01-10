58ffad4959b3ba2c32e622f0a92acdd4e4699cc7
Михаил Смирнов <urine89@mail.ru>
2018-01-11 01:21:16 +0300

    Add connection with App

--- static/arca.js
+++ static/arca.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-import Main from 'static/components/Main';
+import App from 'static/components/App';
 import Project from 'static/components/Project';
 import Projects from 'static/components/Projects';
 
-import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import {Router, Route, IndexRoute} from 'react-router';
 import {Provider} from 'react-redux';
 import store, {history} from 'static/store';
 
 const router = (
     <Provider store={store}>
         <Router history={history}>
-            <Route path='/' component={Main}>
+            <Route path='/' component={App}>
                 <IndexRoute component={Projects}></IndexRoute>
                 <Route path='/project/:id' component={Project}></Route>
             </Route>
---
+++ static/components/App.js
@@ -0,0 +1,20 @@
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+
+import * as actionCreators from 'static/actions/actionCreators';
+
+import Main from 'static/components/Main';
+
+function mapStateToProps(state) {
+    return {
+        projects: state.projects
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(actionCreators, dispatch);
+}
+
+const App = connect(mapStateToProps, mapDispatchToProps)(Main);
+
+export default App;
