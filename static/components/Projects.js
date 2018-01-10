5bbb7e3ede87eaa00c07ee8a1f52e3b524168095
Михаил Смирнов <urine89@mail.ru>
2018-01-11 00:30:03 +0300

    Resolve 404 for Routing

---
+++ static/components/Projects.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router';
+
+
+class Projects extends Component{
+    render(){
+        return (
+            <div>PROJECTs <Link to='/project/tesfasas'>go to</Link></div>
+        )
+    }
+}
+
+export default Projects;
