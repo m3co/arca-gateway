f2912f5dada46e42fe6bde98da8c7b4c0c72bca6
Andres Rios <rianby64@gmail.com>
2018-03-09 18:06:24 +0300

    Update README.js

--- README.js
+++ README.js
@@ -1,14 +1,3 @@
-# ARCA NodeJS for WEB
+# ARCA Proxy (nodeJS)
 
-In order to start the back-server, execute the following:
-```
-npm run back
-```
-
-In order to start the front-server (example), execute the following:
-```
-npm run front
-```
-Then navigate to http://localhost:8080/client.html
-
-Notice that the servers front and back are running under ports 9001 and 9000 respectively.
+Lo que me interesa de este proxy es que nodeJS tiene una biblioteca excelentisima llamada Socket.io y, bueno...
