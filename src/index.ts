fc42adb4099f2a04aac1d806f426f18cda0472b0
Andres Rios <rianby64@gmail.com>
2020-03-10 19:30:05 +0300

    Muestre como probar... eso esta largo

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 
 import { readFileSync } from 'fs';
 import { Socket } from 'net';
-
 import { parse } from 'ini';
-import { v4 as uuidv4 } from 'uuid';
 
 export const config = parse(readFileSync('config.ini', 'utf-8'));
 
-interface Response {
+export interface Response {
     ID: string;
     Context: {
         Source: string
@@ -21,22 +19,5 @@ interface Response {
     }
 }
 
-const arca = new Socket();
+export const arca = new Socket();
 arca.setEncoding('utf-8');
-
-arca.on('data', (data: Buffer) => {
-    const result = JSON.parse(data.toString()) as Response;
-    console.log(result);
-});
-
-arca.connect(22345, 'localhost', (): void => {
-    console.log('connected');
-    const request = {
-        ID: uuidv4(),
-        Method: 'test',
-        Context: {
-            Source: 'test-source'
-        }
-    }
-    arca.write(`${JSON.stringify(request)}\n`);
-});
--- src/testting.test.ts
+++ src/testting.test.ts
@@ -1,8 +1,41 @@
 
+import { arca, Response } from './index';
+import { v4 as uuidv4 } from 'uuid';
 
-import { config } from './index';
+test('whatever', async () => {
 
-test('whatever', () => {
-    console.log(config);
-    expect(true).toBe(true);
+    const id = uuidv4();
+    const fn = (
+        resolve: (value?: Response | PromiseLike<Response>) => void,
+        reject: (reason?: string) => void
+        ) => {
+        arca.on('data', (data: Buffer) => {
+            const result = JSON.parse(data.toString()) as Response;
+            resolve(result);
+            arca.end();
+        });
+    
+        arca.on('connect', (had_error: boolean): void => {
+            if (had_error) {
+                reject('an error');
+                return;
+            }
+            const request = {
+                ID: id,
+                Method: 'test',
+                Context: {
+                    Source: 'test-source'
+                }
+            }
+            arca.write(`${JSON.stringify(request)}\n`);
+        });
+    
+        arca.connect(22345, 'localhost');
+    };
+
+    const t = new Promise<Response>(fn);
+
+    const response = await t;
+    console.log(response);
+    expect(response.ID).toBe(id);
 });
